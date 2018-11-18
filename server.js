982867849a9d17f1106d4523f70b0a6f1590ddb3
unknown <naormordechaix@gmail.com>
2018-11-18 23:57:26 +0200

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const addTeamRoutes = require('./routes/team-routes')
 
-const app = express(); 
+const app = express();
 
 app.use(cors({
     origin: ['http://localhost:3000'],
@@ -13,6 +13,17 @@ app.use(cors({
 app.use(bodyParser.json())
 
 addTeamRoutes(app)
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`phone app listening on port ${PORT}`))
