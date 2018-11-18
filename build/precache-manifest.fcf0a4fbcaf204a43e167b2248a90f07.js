61b8239c9849fac9f66b2873abf4f54968a8180f
unknown <naormordechaix@gmail.com>
2018-11-18 22:56:22 +0200

    add build folder

---
+++ build/precache-manifest.fcf0a4fbcaf204a43e167b2248a90f07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d93a3fb3afbae12be966f1026c1d02b7",
+    "url": "/static/media/background-green.d93a3fb3.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "488aa86f74a6784862c8",
+    "url": "/static/js/main.488aa86f.chunk.js"
+  },
+  {
+    "revision": "baf2fc6bf18ccc634af9",
+    "url": "/static/js/1.baf2fc6b.chunk.js"
+  },
+  {
+    "revision": "488aa86f74a6784862c8",
+    "url": "/static/css/main.8bd93937.chunk.css"
+  },
+  {
+    "revision": "814bfc3ab0d32496b5d5b94e7b804281",
+    "url": "/index.html"
+  }
+];
