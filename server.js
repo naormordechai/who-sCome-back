5fd162f6e1daf4c01c4804959c28fcf87f4f9610
unknown <naormordechaix@gmail.com>
2018-11-19 00:00:59 +0200

    a

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ addTeamRoutes(app)
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('front/build'));
+    app.use(express.static('./build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
   }
 
